4b5e5c187df39e8b583b8fcebbf0413fbc54ae06
xingwei <xingwei@soundai.com>
2020-03-10 16:04:54 +0800

    finish:300_lengthOfLIS

---
+++ src/main/kotlin/com/dynamicProgramming/m300_lengthOfLIS/Solution.kt
@@ -0,0 +1,30 @@
+package com.dynamicProgramming.m300_lengthOfLIS
+
+import kotlin.math.max
+
+/**
+ * Create by xingw on 2020/3/10
+ */
+object Solution {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println(lengthOfLIS(intArrayOf(1, 3, 6, 7, 9, 4, 10, 5, 6)))
+    }
+
+    fun lengthOfLIS(nums: IntArray): Int {
+        val dp = IntArray(nums.size) { 1 }
+
+        for (i in 0 until nums.size)
+            for (j in 0 until i) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = max(dp[i], dp[j] + 1)
+                }
+            }
+
+        var res = 0
+        for (i in dp) {
+            res = max(res, i)
+        }
+        return res
+    }
+}
