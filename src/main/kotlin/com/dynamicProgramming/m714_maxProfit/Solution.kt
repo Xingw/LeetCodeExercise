cdc59f9b9c21ac338422f0c6c3f509208b6f81f3
Xingw <541460835@qq.com>
2020-03-12 20:09:47 +0800

    finish:714 maxProfit

--- src/main/kotlin/com/dynamicProgramming/m714_maxProfit/Solution.kt
+++ src/main/kotlin/com/dynamicProgramming/m714_maxProfit/Solution.kt
@@ -15,7 +15,6 @@ object Solution {
             val temp = dp_i_0
             dp_i_0 = max(dp_i_0, dp_i_1 + prices[i])
             dp_i_1 = max(dp_i_1, temp - prices[i]-fee)
-            prices
         }
         return dp_i_0
     }
